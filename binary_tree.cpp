3744122968120f27dbb125c08710119e09967e1b
Kartik Jangra <kartikjangra416@gmail.com>
2025-02-11 09:42:48 +0530

    tree
    commit binary tree

--- binary_tree.cpp
+++ binary_tree.cpp
@@ -54,13 +54,87 @@ void levelOrderTraversal(node* root){
     }
     
 }
+}
+void inorder(node*root){
+    if(root==NULL){
+        return;
+    }
+    inorder(root->left);
+    cout<<root->data<<" ";
+    inorder(root->right);
+
+}
+void preorder(node*root){
+    if(root==NULL){
+        return;
+    }
+
+    cout<<root->data<<" ";
+    preorder(root->left);
+    
+    preorder(root->right);
+
+}
+void postorder(node*root){
+    if(root==NULL){
+        return;
+    }
+    
+    
+    postorder(root->left);
+    
+    postorder(root->right);
+    cout<<root->data<<" ";
+
+}
+
+void buildfromlevelordertraversal(node*& root){
+    queue<node*> q;
+    
+    cout<<"enter data for root:"<<endl;
+    int data;
+    cin>>data;
+    root=new node(data);
+    q.push(root);
+
+    while (!q.empty())
+    {
+        node*temp=q.front();
+        q.pop();
+        cout<<"enter the left node data:"<<temp->data<<endl;
+        int leftdata;
+        cin>>leftdata;
+        if(leftdata!=-1){
+            temp->left=new node(leftdata);
+            q.push(temp->left);
+        }
+        cout<<"enter the right node data:"<<temp->data<<endl;
+        int rightdata;
+        cin>>rightdata;
+        if(rightdata!=-1){
+            temp->right=new node(rightdata);
+            q.push(temp->right);
+        }
+    }
+    
 
 }
 int main(){
     node* root=NULL; 
-    root=buildTree(root); 
-    // data to enter ->1 3 7 -1 -1 11 -1 -1 5 17 -1 -1 -1
-    cout<<"your binary tree is : "<<endl;
-    levelOrderTraversal(root);
 
+    buildfromlevelordertraversal(root);
+    levelOrderTraversal(root);
+    // root=buildTree(root); 
+    // // data to enter ->1 3 7 -1 -1 11 -1 -1 5 17 -1 -1 -1
+    // cout<<"your binary tree is : "<<endl;
+    // levelOrderTraversal(root);
+    // cout<< "inorder traversal printing"<<endl;
+    // inorder(root);
+    // cout<<endl;
+    // cout<< "preorder traversal printing"<<endl;
+    // preorder(root);
+    // cout<<endl;
+    // cout<< "postorder traversal printing"<<endl;
+    // postorder(root);
+    // cout<<endl;
 }
